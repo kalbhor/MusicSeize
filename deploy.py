a2c651f62173e272efc685e7808073f813024043
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-04-14 03:30:36 +0530

    changes in design, prettyfied code

--- deploy.py
+++ deploy.py
@@ -1,13 +1,20 @@
 """
 A simple flask application built for a webapp that provides
-.mp3 files with metadata. 
+.mp3 files with metadata.
 
 The heart of this webapp is based on musictools - A library specifically
 built for this webapp. Musictools handles downloading the music from youtube
 and handles metadata by searching from spotify and attaching using mutagen.
 
-spotify requests are handled by spotify-dl
+spotify requests are handled by spotipy
 youtube downloading is handled by youtube-dl
+
+Tests for this flask app are mostly trivial (I know testing is important)
+Since anything that can go wong, will go wrong with the musictools, it is
+tested.
+
+I am very sorry for the pathetic html code in the templates. I'm not a 
+html guy.
 """
 
 import sys
@@ -15,64 +22,60 @@ import logging
 import os
 import binascii
 from musictools import musictools
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, \
                   url_for, send_file, after_this_request
 
-# Initialize the Flask application
 app = Flask(__name__)
 app.secret_key = binascii.hexlify(os.urandom(24))
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/counter.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def download_song(input_title, input_url):
-    """
-    Method to use musictools to download song
-    and add metadata, return details about song
-    to display on webpage
 
-    'tmp/' is a location where heroku allows storage for a single request.
-    (tmp cannot be used for permanent storage)
-    """
-    musictools.download_song(input_url, input_title, dl_directory = 'tmp/')
-    artist, album, song_title, albumart = musictools.get_metadata(input_title)
-    album_src = musictools.add_albumart(input_title + '.mp3', song_title, albumart)
-    musictools.add_metadata(input_title + '.mp3', song_title, artist, album)
 
-    result = {
-    'artist': artist,
-    'album' : album,
-    'song' : song_title,
-    'art': album_src,
-    } # Details to display on webpage
+##########################################################################
+############ Database for counting number of hits ########################
+##########################################################################
 
-    return input_title + '.mp3', song_title, result
+db = SQLAlchemy(app)
 
-# Define a route for the default URL, which loads the form
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+
+    def __init__(self):
+        self.count = 0
+
+
+##########################################################################
+########################## Flask Routes ##################################
+##########################################################################
+
 @app.route('/')
 def index():
     """
     Home page, with a form that accepts song name as query
     and searches song on youtube.com
     """
-    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    """
-    As the name suggests, a standard about page with 
-    details on the website
-    """
-    return render_template('about.html')
+    v = Visit.query.first() # To fetch count; hint : see download()
+
+    return render_template('index.html', count=v.count)
+
 
 @app.route('/songlist/', methods=['POST'])
 def songlist():
     """
     Searches query on youtube.com and displays on webpage.
-    Music titles are represented as radio buttons allowing 
+    Music titles are represented as radio buttons allowing
     the user to choose 1 song to download.
     """
+
     song_name = request.form['songname']
     youtube_list = musictools.get_song_urls(song_name) # youtube_list is an ordered dict
+
     return render_template('form_action.html', youtube_list=youtube_list[:5])
 
 
@@ -83,32 +86,75 @@ def process():
     and call download_song method to handle downloading
     and metadata extraction & attaching.
     """
+
+    v = Visit.query.first()
+    v.count += 1             # Increment number of downloaded songs
+    db.session.commit()
+
     if not os.path.exists('/tmp'):
-        os.mkdirs('/tmp')
+        os.mkdir('/tmp')
+
     input_title = request.form['title']
     input_url = request.form['url']
     file_path, song_title, result = download_song(input_title, input_url)
 
     return render_template('process.html', path=file_path, song=song_title, result=result)
 
-@app.route('/download/<path>/<song>/', methods=['POST','GET'])
+
+@app.route('/download/<path>/<song>/', methods=['POST', 'GET'])
 def download(path=None, song=None):
     """
     Explicitly delete song from server after user downloads it,
-    (Heroku does this automatically after request expires or if user doesn't 
+    (Heroku does this automatically after request expires or if user doesn't
     respond)
     """
-    @after_this_request
+
+    @after_this_request # Delete music file after request
     def delete_file(response):
-        try:
-            os.remove(os.path.join('tmp','{}'.format(path)))
-        except Exception as e:
-            print(e)
+        os.remove(os.path.join('tmp', '{}'.format(path)))
         return response
 
     return send_file('tmp/'+path, as_attachment=True, attachment_filename=str(song+'.mp3'))
 
 
-if __name__=='__main__':
-    app.run(debug=True) # For locally running the application
+@app.route('/about')
+def about():
+    """
+    As the name suggests, a standard about page with
+    details on the website
+    """
+    return render_template('about.html')
 
+
+
+##########################################################################
+########################## Download Methods ##############################
+##########################################################################
+
+def download_song(input_title, input_url):
+    """
+    Method to use musictools to download song
+    and add metadata, return details about song
+    to display on webpage
+
+    'tmp/' is a location where heroku allows storage for a single request.
+    (tmp cannot be used for permanent storage)
+    """
+
+    musictools.download_song(input_url, input_title, dl_directory='tmp/')
+    artist, album, song_title, albumart = musictools.get_metadata(input_title)
+    album_src = musictools.add_albumart(input_title + '.mp3', song_title, albumart)
+    musictools.add_metadata(input_title + '.mp3', song_title, artist, album)
+
+    result = {
+        'artist': artist,
+        'album' : album,
+        'song' : song_title,
+        'art': album_src,
+    } # Details to display on webpage
+
+    return input_title + '.mp3', song_title, result
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # For locally running the application
