95a0ff85980bb33d7326eb2d4be394a99ad088fc
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-04-23 04:53:13 +0530

    Refactored code

--- deploy.py
+++ deploy.py
@@ -1,15 +1,18 @@
 """
 A simple flask application built for a webapp that provides
 .mp3 files with metadata.
+
 The heart of this webapp is based on musictools - A library specifically
-built for this webapp. Musictools handles downloading the music from youtube
+built for this webapp.
+
+Musictools handles downloading the music from youtube
 and handles metadata by searching from spotify and attaching using mutagen.
+
 spotify requests are handled by spotipy
 youtube downloading is handled by youtube-dl
+
 Tests for this flask app are mostly trivial (I know testing is important)
 musictools is the core application and it is tested.
-I am very sorry for the pathetic html code in the templates. I'm not a 
-frontend person.
 """
 
 import sys
@@ -19,7 +22,8 @@ import binascii
 from musictools import musictools
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, \
-                  url_for, send_file, after_this_request
+                  url_for, send_file, abort, \
+                  after_this_request
 
 ##########################################################################
 ############################# Configurations #############################
@@ -36,6 +40,7 @@ app.logger.addHandler(file_handler)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+EXT = 'mp3' # File extension
 
 ##########################################################################
 ############################# Database ###################################
@@ -63,7 +68,7 @@ def index():
     and searches song on youtube.com
     """
 
-    v = Visit.query.first()
+    v = Visit.query.first() # Get last downloaded song
 
     return render_template('index.html', count=v.count, song_name=v.song_name)
 
@@ -72,11 +77,16 @@ def index():
 def songlist():
     """
     Searches query on youtube.com and displays on webpage.
+
     Music titles are represented as radio buttons allowing
     the user to choose 1 song to download.
     """
 
     song_name = request.form['songname']
+
+    if song_name == '': # If user submits form without input
+        abort(400)
+
     youtube_list = musictools.get_song_urls(song_name) # youtube_list is an ordered dict
 
     return render_template('form_action.html', youtube_list=youtube_list[:5])
@@ -86,11 +96,14 @@ def songlist():
 def process():
     """
     Process the query and download song on server
-    and call download_song method to handle downloading
+
+    Call download_song method to handle downloading
     and metadata extraction & attaching.
     """
+
     print('Started process')
     sys.stdout.flush()
+
     if not os.path.exists('/tmp'):
         os.mkdir('/tmp')
 
@@ -102,7 +115,10 @@ def process():
     def write_to_db(response):
         v = Visit.query.first()
         v.count += 1             # Increment number of downloaded songs
-        v.song_name = '{} - {}'.format(result['artist'], result['song']) 
+        if result['artist']:
+            v.song_name = '{} - {}'.format(result['artist'], result['title']) 
+        else:
+            v.song_name = '{}'.format(result['title'])
         db.session.commit()
 
         return response
@@ -110,8 +126,8 @@ def process():
     return render_template('process.html', path=file_path, result=result)
 
 
-@app.route('/download/<path>/<song>/', methods=['POST', 'GET'])
-def download(path=None, song=None):
+@app.route('/download/<path>/<title>/', methods=['POST', 'GET'])
+def download(path=None, title=None):
     """
     Explicitly delete song from server after user downloads it,
     (Heroku does this automatically after request expires or if user doesn't
@@ -120,10 +136,10 @@ def download(path=None, song=None):
 
     @after_this_request # Delete music file after request
     def delete_file(response):
-        os.remove(os.path.join('tmp', path))
+        os.remove(os.path.join('tmp',path))
         return response
 
-    return send_file(os.path.join('tmp',path), as_attachment=True, attachment_filename=str(song+'.mp3'))
+    return send_file(os.path.join('tmp',path), as_attachment=True, attachment_filename='{}.{}'.format(title, EXT))
 
 
 @app.route('/about')
@@ -150,7 +166,7 @@ def caution():
 
 
 ##########################################################################
-########################## Download Methods ##############################
+########################## Other Methods #################################
 ##########################################################################
 
 def download_song(input_title, input_url):
@@ -161,22 +177,31 @@ def download_song(input_title, input_url):
     'tmp/' is a location where heroku allows storage for a single request.
     (tmp cannot be used for permanent storage)
     """
+    file_path = 'tmp/{}'.format(input_title)
+    metadata = []
 
-    musictools.download_song(input_url, input_title, dl_directory='tmp/')
-    artist, album, song_title, albumart = musictools.get_metadata(input_title)
-    album_src = musictools.add_albumart(input_title + '.mp3', song_title, albumart)
-    musictools.add_metadata(input_title + '.mp3', song_title, artist, album)
+
+    musictools.download_song(input_url, file_path) # No need for ext here, download_song does that automatically
+    try: 
+        metadata = musictools.get_metadata(input_title)
+    except IndexError:
+        metadata = [None , None, input_title, None]
+        print('Metadata search failed for {}'.format(input_title))
+        sys.stdout.flush()
+    else:
+        musictools.add_metadata('{}.{}'.format(file_path,EXT), metadata[2], metadata[0], metadata[1])
+        musictools.add_album_art('{}.{}'.format(file_path,EXT), metadata[3])
 
 
     result = {
-        'artist': artist,
-        'album' : album,
-        'song' : song_title,
-        'art': album_src,
+        'artist': metadata[0],
+        'album' : metadata[1],
+        'title' : metadata[2],
+        'art': metadata[3],
     } # Details to display on webpage
 
 
-    return input_title + '.mp3', result
+    return '{}.{}'.format(input_title,EXT), result
 
 
 if __name__ == '__main__':
